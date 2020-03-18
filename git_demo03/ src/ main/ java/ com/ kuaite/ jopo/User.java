c9b7c3964f410bf75f7f5ce4510703ccf92de77b
YuTaoSun <958006546@qq.com>
2020-03-18 10:30:32 +0800

    Merge branch 'master' of D:\ideawss\Git with conflicts.

--- git_demo03/ src/ main/ java/ com/ kuaite/ jopo/User.java
+++ git_demo03/ src/ main/ java/ com/ kuaite/ jopo/User.java
@@ -3,7 +3,7 @@
 public class User {
     public static void main(String[] args) {
         System.out.println(6666);
-System.out.println(“你不好”);
-
+        System.out.println(“你不好”);
+        System.out.println("你好");
     }
 }
