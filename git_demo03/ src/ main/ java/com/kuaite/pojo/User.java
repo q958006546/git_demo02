ebf2880d48114db0e25ddf7cbf91fb1af7971669
YuTaoSun <958006546@qq.com>
2020-03-18 10:56:32 +0800

    0.6v

---
+++ git_demo03/ src/ main/ java/com/kuaite/pojo/User.java
@@ -0,0 +1,9 @@
+package com.kuaite.pojo;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println(6666);
+        System.out.println("你好");
+        System.out.println("6666");
+    }
+}
