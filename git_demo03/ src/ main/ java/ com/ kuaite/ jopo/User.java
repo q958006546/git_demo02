388101c35893d9baf18077651530b34235bd6e80
YuTaoSun <958006546@qq.com>
2020-03-18 10:22:34 +0800

    0.5v

--- git_demo03/ src/ main/ java/ com/ kuaite/ jopo/User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    public static void main(String[] args) {
-        System.out.println(6666);
-        System.out.println(6666);
-        System.out.println(6666);
-        系统。出来。println（7777）;
-哈哈哈
-    }
-}
